a62de0822bcfda4c6bc977127701329cb5f1e4fe
Yuvraj-Ganshi <85454673+yuvraj0651@users.noreply.github.com>
2022-08-28 12:17:52 +0530

    Create input.java

---
+++ input.java
@@ -0,0 +1,20 @@
+package com.YuvrajGanshi;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class input {
+
+	public static void main(String[] args) {
+		
+		int num;
+		System.out.println("Enter any number");
+		
+		Scanner s =new Scanner(System.in);
+		
+	    num = s.nextInt();
+		System.out.println("The Entered Number is:" +num);
+
+	}
+
+}
